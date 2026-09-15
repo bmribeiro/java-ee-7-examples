f1072bf2e581bc1beda3e57f0e054f9b6420ea48
Bruno Ribeiro <25556056+bmribeiro@users.noreply.github.com>
2026-09-15 18:19:13 +0100

    java-ee-7-examples

---
+++ rest/src/main/java/pt/brunoribeiro/examples/rest/HelloResource.java
@@ -0,0 +1,36 @@
+package pt.brunoribeiro.examples.rest;
+
+import javax.json.Json;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class HelloResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello Java EE 7 REST!";
+    }
+
+    @GET
+    @Path("/{name}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String helloName(@PathParam("name") String name) {
+        return "Hello " + name + "!";
+    }
+
+    @GET
+    @Path("/json/{name}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String helloJson(@PathParam("name") String name) {
+
+        return Json.createObjectBuilder()
+                .add("message", "Hello " + name + "!")
+                .build()
+                .toString();
+    }
+}
---
+++ rest/src/main/java/pt/brunoribeiro/examples/rest/RestApplication.java
@@ -0,0 +1,8 @@
+package pt.brunoribeiro.examples.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class RestApplication extends Application {
+}
